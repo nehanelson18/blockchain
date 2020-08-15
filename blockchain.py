1765e555020435061318fedf60b916240b53905e
Neha Nelson <nehanelson@Nehas-MBP.hsd1.nj.comcast.net>
2020-08-15 16:03:10 -0400

    added Proof of Work

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
+
 
 class Blockchain(object):
     def __init__(self):
@@ -21,20 +23,19 @@ class Blockchain(object):
         """
 
         block = {
-            'index' : len(self.chain) +1,
-            'timestamp' : time(),
-            'transactions' : self.current_transactions,
-            'proof' : prrof,
-            'previous_hash' : previous_hash or self.hash(self.chain[-1]),
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': prrof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
 
         }
 
         # Reset the current list of transactions
-        self.current_transactions =[]
+        self.current_transactions = []
 
         self.chain.append(block)
         return block
-
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -47,14 +48,18 @@ class Blockchain(object):
         """
 
         self.current_transaction.append({
-            'sender' : sender,
-            'recipient': recipient, 
-            'amount' : amount, 
-        }) 
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
 
         return self.last_block['index'] + 1
-        #Adds a new transaction to the list of transactions 
-        pass
+        # Adds a new transaction to the list of transactions
+
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        return self.chain[-1]
 
     @staticmethod
     def hash(block):
@@ -68,10 +73,29 @@ class Blockchain(object):
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
-        pass
 
-    @property
-    def last_block(self):
-        # Returns the last block in the chain
-        return self.chain[-1]
-        
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm
+        - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int> 
+        """
+        proof - 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """ 
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
